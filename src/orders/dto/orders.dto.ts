ccace21c13714f94ed5cfc671e8c0beafdfcad7a
mohamad <m.aleik@darsoft.net>
2021-10-12 23:05:40 +0300

    Remove unused imports

--- src/orders/dto/orders.dto.ts
+++ src/orders/dto/orders.dto.ts
@@ -1,8 +1,3 @@
-import { Product } from 'src/schemas/product.schema';
-import { Cart } from '../../schemas/order.schema';
-import { ValidateNested } from 'class-validator';
-import { Type } from 'class-transformer';
-
 export interface OrdersDto {
   ordersList: {
     productId: string;
