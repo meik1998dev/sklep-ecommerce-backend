fd67fad20a20bcec46476147f5b1d422879f9461
mohamad <m.aleik@darsoft.net>
2021-10-21 19:16:40 +0300

    update routes and update remove product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,7 +11,7 @@ import { SellersService } from 'src/sellers/sellers.service';
 import { ProductDto } from './dto/product.dto';
 import { ProductsService } from './products.service';
 
-@Controller('my_products')
+@Controller('products')
 export class ProductsController {
   constructor(
     private readonly productService: ProductsService,
@@ -19,7 +19,7 @@ export class ProductsController {
   ) {}
 
   // Create product by seller
-  @Post('add')
+  @Post()
   async CreateProduct(@Body() productDto: ProductDto) {
     // find the seller that will add the product
     const seller = await this.sellerService.findById(
@@ -32,17 +32,32 @@ export class ProductsController {
     );
     //update seller's products array
     seller.products.push(createdProduct);
-    return await seller.save();
+
+    await seller.save();
+
+    return (await seller.populate('products')).products;
   }
 
   // Update product informations
   @Put(':id')
-  updateProduct(@Body() ProductDto: ProductDto) {
-    return this.productService.update('61360b37e22bb325f4b64781', ProductDto);
+  updateProduct(@Body() ProductDto: ProductDto, @Param('id') id: string) {
+    return this.productService.update(id, ProductDto);
   }
 
   @Delete(':id')
-  removeProduct(@Param('id') id: string) {
-    return this.productService.delete(id);
+  async removeProduct(@Param('id') id: string) {
+    const seller = await this.sellerService.findById(
+      '6135207bc44d401934aad40d',
+    );
+
+    const newArr = seller.products.filter((product: any) => {
+      return product._id != id;
+    });
+
+    seller.products = newArr;
+
+    await seller.save();
+
+    return (await seller.populate('products')).products;
   }
 }
