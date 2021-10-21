ec40119176d610514eb70fd1019db96cbf410c64
mohamad <m.aleik@darsoft.net>
2021-10-21 22:08:02 +0300

    remove unused function argument

--- src/sellers/sellers.controller.ts
+++ src/sellers/sellers.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { OrdersService } from 'src/orders/orders.service';
 import { ProductsService } from 'src/products/products.service';
 import { CreateSellerDto } from './dto/create-seller.dto';
@@ -19,7 +19,6 @@ export class SellersController {
 
   @Put('profile')
   updateProfile(
-    @Param('id') id: string,
     @Body() createSellerDto: CreateSellerDto,
   ) {
     return this.sellerService.update(
@@ -31,11 +30,6 @@ export class SellersController {
   @Get('profile')
   getSellerProfile() {
     return this.sellerService.findById('6165e6e7dfa6606436c92d42');
-  }
-
-  @Get('my_products')
-  getAllProducts() {
-    return this.productService.findAllBySellerId('6135207bc44d401934aad40d');
   }
 
   @Get('orders')
